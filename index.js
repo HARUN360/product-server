7f2a43c64dd7cc502653a2814fdc6a8c2178369e
HARUN360 <mdharunorroshid301204@gmail.com>
2024-08-18 13:12:55 +0600

    second commit

--- index.js
+++ index.js
@@ -1,81 +1,124 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const app = express();
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 5000;
-
-// middleware
-app.use(cors());
-app.use(express.json());
-
-
-
-
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+var cors = require('cors')
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            // "https://assignment-12-humayun-ph-b9.netlify.app",
+        ],
+        credentials: true,
+    })
+);
+
+app.use(express.json())
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9b6e9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sf3cbqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
+
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    const productCollection = client.db('productDB').collection('products');
+        
+        const phonesCollection = client.db('productDB').collection('products');;
 
-    app.get('/products', async (req, res) => {
-      const page = parseInt(req.query.page)
-      const size = parseInt(req.query.size)
-      console.log('pagination query', page, size);
-      const result = await productCollection.find()
-      .skip(page * size)
-      .limit(size)
-      .toArray();
-      res.send(result);
 
-    })
+        app.get('/phones', async (req, res) => {
+    // const { img, name, seller, ratings, price, category } = product;
 
-    app.post('/productByIds', async(req, res) => {
-      const ids = req.body;
-      const idsWithObjectId = ids.map(id => new ObjectId(id));
-      const query = {
-        _id: {
-          $in: idsWithObjectId
-        }
-      }
-      // console.log(idsWithObjectId);
-      const result = await productCollection.find(query).toArray();
-      res.send(result)
-    })
+            const brand = req.query.seller; // Get brand from query params
+            const category = req.query.category; // Get category from query params
+            const name = req.query.name; // Get category from query params
+            console.log({ brand, category, name });
+            // const productname=name.toLocaleLowerCase()
 
-    app.get('/productsCount', async (req, res) => {
-      const count = await productCollection.estimatedDocumentCount();
-      res.send({ count })
-    })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+            let query = {};
+
+            if (brand && brand !== 'null') {
+                query.brand = brand;
+            }
+
+            if (category && category !== 'null') {
+                query.category = category;
+            }
+
+            if (name) {
+                query.productname = { $regex: name, $options: "i" };
+            }
+
+
+
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const allProducts = await phonesCollection.find()
+
+                .skip(page * size)
+                .limit(size)
+                .toArray();
+
+
+
+
+
+            try {
+
+                // const result = await phonesCollection.find(query).toArray();
+                // res.send(result);
+                const count = await phonesCollection.countDocuments()
+                // console.log(allProducts, count)
+
+                res.send({
+                    phones: allProducts,
+                    count: count
+                })
+            } catch (error) {
+                console.error(error);
+                res.status(500).send({ message: "Internal Server Error" });
+            }
+        });
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-  res.send('john is busy shopping')
+    res.send('Gadged Zone server is running')
 })
 
 app.listen(port, () => {
-  console.log(`ema john server is running on port: ${port}`);
+    console.log(`Server is Running on port ${port}`)
 })
